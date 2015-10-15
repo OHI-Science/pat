7278d03b3399bc8089c83c84e29da1c424a9f883
Julia Stewart Lowndes <jstewart@nceas.ucsb.edu>
2015-10-15 15:41:32 -0700

    removed @dev

--- subcountry2014/install_ohicore.r
+++ subcountry2014/install_ohicore.r
@@ -23,7 +23,7 @@ for (p in c('devtools')){
 
 # install packages
 install_github('ohi-science/rCharts')
-install_github('ohi-science/ohicore@dev')
+install_github('ohi-science/ohicore')
 
 # Note: you will get warning messages like the following; this is nothing to worry about.
 # Warning: replacing previous import by ‘plyr::mutate’ when loading ‘ohicore’
