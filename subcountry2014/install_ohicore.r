6649d1707c8e86bf5d8b72050a6d90786d80a709
jules32 <jstewart@nceas.ucsb.edu>
2016-05-23 14:15:29 -0700

    debugging with @Melsteroni, updated install

--- subcountry2014/install_ohicore.r
+++ subcountry2014/install_ohicore.r
@@ -5,25 +5,17 @@
   # 2. Download and update RStudio. RStudio is optional, but highly recommended. Find the latest version at http://www.rstudio.com/products/rstudio/download
   # 3. Run the following as a one-time install:
 
-# remove old packages
-for (p in c('ohicore','ohigui','rCharts')){
+for (p in c('ohicore')){
   if (p %in% rownames(installed.packages())){
     lib = subset(as.data.frame(installed.packages()), Package==p, LibPath, drop=T)
     remove.packages(p, lib)
   }
 }
 
-# install dependencies
-for (p in c('devtools')){
-  if (!require(p, character.only=T)){
-    install.packages(p)
-    require(p, character.only=T)
-  }
-}
-
 # install packages
-install_github('ohi-science/rCharts')
-install_github('ohi-science/ohicore')
+devtools::install_github('ohi-science/ohicore@dev_resil')
+
+
 
 # Note: you will get warning messages like the following; this is nothing to worry about.
 # Warning: replacing previous import by ‘plyr::mutate’ when loading ‘ohicore’
