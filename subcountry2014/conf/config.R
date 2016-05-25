242f82191edca27f373740ba9a477b7c9a491840
jules32 <jstewart@nceas.ucsb.edu>
2016-05-25 15:39:06 -0700

    checking CHL with ohicore@dev_resil, a la: https://github.com/OHI-Science/issues/issues/659#issuecomment-221467641

--- subcountry2014/conf/config.R
+++ subcountry2014/conf/config.R
@@ -5,21 +5,22 @@ layer_region_areas      = 'rgn_area'
 
 # pressures & resilience matrices ----
 # components describe the layer and level with which to aggregate resilience and pressures matrices for goals with categories
-resilience_components = list('NP'  = c('layer'='np_harvest_product_weight' , 'level'='region_id-category'),  # old: rnk_np_product_weight
-                             'CS'  = c('layer'='cs_habitat_extent'         , 'level'='region_id'),
-                             'CP'  = c('layer'='cp_habitat_extent_rank'    , 'level'='region_id'),           # old: rnk_cp_habitat_extent
-                             'HAB' = c('layer'='hab_presence'              , 'level'='region_id'))
-pressures_components  = list('NP'  = c('layer'='np_harvest_product_weight' , 'level'='region_id-category'),
-                             'CS'  = c('layer'='cs_habitat_extent'         , 'level'='region_id'),
-                             'CP'  = c('layer'='cp_habitat_extent_rank'    , 'level'='region_id'),
-                             'LIV' = c('layer'='le_sector_weight'          , 'level'='region_id'),
-                             'ECO' = c('layer'='le_sector_weight'          , 'level'='region_id'),
-                             'HAB' = c('layer'='hab_presence'              , 'level'='region_id'))
+resilience_components = list('NP'  = 'np_harvest_product_weight',
+                             'CS'  = 'cs_habitat_extent',
+                             'CP'  = 'cp_habitat_extent_rank',
+                             'HAB' = 'hab_presence')
+pressures_components  = list('NP'  = c('layer'='np_harvest_product_weight'),
+                             'CS'  = c('layer'='cs_habitat_extent'        ),
+                             'CP'  = c('layer'='cp_habitat_extent_rank'   ),
+                             'LIV' = c('layer'='le_sector_weight'         ),
+                             'ECO' = c('layer'='le_sector_weight'         ),
+                             'HAB' = c('layer'='hab_presence'             ))
 pressures_categories = list(environmental=c('po','hd','fp','sp','cc'), social='ss')
-resilience_categories = c('environmental', 'regulatory', 'social')
 
 # constants
-pressures_gamma = 0.5
+pressures_gamma  = 0.5 # The relative importance of social vs. ecological pressures   (pressure   = gamma * ecological + (1-gamma) * social)
+resilience_gamma = 0.5 # The relative importance of social vs. ecological resiliences (resilience = gamma * ecological + (1-gamma) * social)
+
 goal_discount = 1.0
 goal_beta = 0.67
 default_trend = 0
