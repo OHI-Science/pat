4413d8a2c39dba3048d76dd5041d32930f272cf6
jules32 <jstewart@nceas.ucsb.edu>
2016-08-04 17:52:13 +0100

    testing calculate_scores.r, needs updating

--- install_ohicore.r
+++ install_ohicore.r
@@ -1,20 +1,26 @@
-# install_ohicore.r
+## install_ohicore.r
 
-# Instructions to sucessfully work with the OHI Toolbox
-  # 1. Download and update R. Find the latest version at http://cran.r-project.org.
-  # 2. Download and update RStudio. RStudio is optional, but highly recommended. Find the latest version at http://www.rstudio.com/products/rstudio/download
-  # 3. Run the following as a one-time install:
+## install_ohicore.r installs the most current version of the `ohicore` R. package.
+## You will need to only do this once. `ohicore` requires recent versions of R and RStudio.
+## 1. Update R. Download the latest version at http://cran.r-project.org.
+## 2. Update RStudio. Download the latest version at http://www.rstudio.com/products/rstudio/download
+## 3. Source or run this script.
 
-p = 'ohicore'
-if (p %in% rownames(installed.packages())){
-  lib = subset(as.data.frame(installed.packages()), Package==p, LibPath, drop=T)
-  remove.packages(p, lib)
+## delete any existing version of `ohicore`
+for (p in c('ohicore')){
+  if (p %in% rownames(installed.packages())){
+    lib = subset(as.data.frame(installed.packages()), Package==p, LibPath, drop=TRUE)
+    remove.packages(p, lib)
+  }
 }
 
-# install packages
-devtools::install_github('ohi-science/ohicore@dev_resil')
+## install dependencies
+for (p in c('devtools', 'git2r')){
+  if (!require(p, character.only=TRUE)){
+    install.packages(p); require(p, character.only=T)
+  }
+}
 
+## install most current version of ohicore from github. Warnings are fine, but make sure there are no errors.
+devtools::install_github('ohi-science/ohicore')
 
-
-# Note: you will get warning messages like the following; this is nothing to worry about.
-# Warning: replacing previous import by ‘plyr::mutate’ when loading ‘ohicore’
--- subcountry2014/calculate_scores.R
+++ subcountry2014/calculate_scores.R
@@ -1,21 +1,22 @@
-library(devtools)
-devtools::install_github("ohi-science/ohicore@dev_resil") # when testing changes to ohicore
+## calculate_scores.R
 
-# load required libraries
-suppressWarnings(require(ohicore))
+## calculate_scores.R ensures all files are properly configured and calculates OHI scores.
+## - configure_toolbox.r ensures your files are properly configured. It is a script in your repository.
+## - CalculateAll() calculates OHI scores. It is a function in the `ohicore` R package
+##   (this can be written in R as `ohicore::CalculateAll()`).
 
-# set working directory to the scenario directory, ie containing conf and layers directories
-setwd('subcountry2014')
+## When you begin, configure_toolbox.r and CalculateAll() will calculate scores using
+## the 'templated' data and goal models provided. We suggest you work
+## goal-by-goal as you prepare data in the prep folder and develop goal models
+## in functions.r. Running configure_toolbox.r and a specific goal model line-by-line
+## in functions.R is a good workflow.
 
-# load scenario configuration
-conf = Conf('conf')
+## run the configure_toolbox.r script to check configuration
+source('~/github/chl/subcountry2014/configure_toolbox.r')
 
-# run checks on scenario layers
-CheckLayers('layers.csv', 'layers', flds_id=conf$config$layers_id_fields)
+## calculate scenario scores
+scores = ohicore::CalculateAll(conf, layers)
 
-# load scenario layers
-layers = Layers('layers.csv', 'layers')
-
-# calculate scenario scores
-scores = CalculateAll(conf, layers)
+## save scores as scores.csv
 write.csv(scores, 'scores.csv', na='', row.names=F)
+
