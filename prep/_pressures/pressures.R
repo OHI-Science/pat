97701981c1ee5bc54aa5fedcc2c623bc4b86f84e
Vanessa Pizarro <va.pizarropizarro@gmail.com>
2022-11-09 18:53:28 -0300

    actualización presiones

--- prep/_pressures/pressures.R
+++ prep/_pressures/pressures.R
@@ -1,25 +1,41 @@
 library(dplyr)
 library(readxl)
 
-meta<-read_excel("prep/_pressures/presiones.xlsx", sheet = "AO")
+meta<-read_excel("prep/_pressures/presiones.xlsx", sheet = "CP")
+m2<- meta %>% filter (element == "BM")
+
+mode <- function(x) {
+  if(x = NA) return(NA)
+  return(as.numeric(names(which.max(table(x)))))
+}
 
 
-m1<- meta %>%
-  dplyr::summarise( po_chemicals = round(mean(po_chemicals,  na.rm = TRUE)),
-                    po_pathogens = round(mean(po_pathogens,  na.rm = TRUE)),
-                    po_pathogens_fan = round(mean(po_pathogens_fan,  na.rm = TRUE)),
-                    po_nutrients = round(mean(po_nutrients,  na.rm = TRUE)),
-                    po_trash = round(mean(po_trash,  na.rm = TRUE)),
-                    sp_alien = round(mean(sp_alien,  na.rm = TRUE)),
-                    hd_infa = round(mean(hd_infa,  na.rm = TRUE)),
-                    hd_coastal = round(mean(hd_coastal,  na.rm = TRUE)),
-                    hd_traffic = round(mean(hd_traffic,  na.rm = TRUE)),
-                    fp_ilegal = round(mean(fp_ilegal,  na.rm = TRUE)),
-                    fp_unstt = round(mean(fp_unstt,  na.rm = TRUE)),
-                    cs_sst = round(mean(cs_sst,  na.rm = TRUE)),
-                    cc_acid = round(mean(cc_acid,  na.rm = TRUE)),
-                    cc_slr = round(mean(cc_slr,  na.rm = TRUE)),
-                    ss_wgi = round(mean(ss_wgi,  na.rm = TRUE)))
+m1<- m2 %>%
+  dplyr::summarise( po_chemicals = mode(na.omit(po_chemicals)),
+                    po_pathogens = mode(na.omit(po_pathogens)),
+                    po_pathogens_fan = mode(na.omit(po_pathogens_fan)),
+                    po_nutrients = mode(na.omit(po_nutrients)),
+                    po_trash = mode(na.omit(po_trash)),
+                    sp_alien = mode(na.omit(sp_alien)),
+                    hd_infa = mode(na.omit(hd_infa)),
+                    hd_coastal = mode(na.omit(hd_coastal)),
+                    hd_traffic = mode(na.omit(hd_traffic)),
+                    hd_density = mode(na.omit(hd_density)),
+                    fp_ilegal = mode(na.omit(fp_ilegal)),
+                    fp_unstt = mode(na.omit(fp_unstt)),
+                    cs_sst = mode(na.omit(cs_sst)),
+                    cc_acid = mode(na.omit(cc_acid)),
+                    cc_slr = mode(na.omit(cc_slr)),
+                    ss_wgi = mode(na.omit(ss_wgi))) %>%
+  mutate(goal = "AO", element) %>%
+  #mutate(element = NA) %>%
+  select(goal, element, po_chemicals , po_pathogens, po_pathogens_fan, po_nutrients,
+         po_trash, sp_alien , hd_infa , hd_coastal , hd_traffic, hd_density,
+         fp_ilegal,  fp_unstt, cs_sst,  cc_acid,  cc_slr , ss_wgi)
+
+
+matriz<- rbind(matriz, m1)
+
 
 
 
